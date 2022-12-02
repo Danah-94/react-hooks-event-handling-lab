57b1f39aa803e0624e6cb5f72be168db2e806a92
Danah <osta.danah@gmail.com>
2022-12-02 19:30:50 +0200

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+
+    function handleChange(){
+        return console.log('Entering password...');
+    }
+
     return (
-        <div></div>
+        <div>
+            <input onChange={handleChange} type="password" name="password" placeholder="Enter password ..."/>
+        </div>
     )
 }
 
